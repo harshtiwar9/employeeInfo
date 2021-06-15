daac168aee20d3b048e2bea6b6b6e533e550a0b4
Harsh Tiwar <harsh.tiwar9@gmail.com>
2021-06-15 12:33:34 -0400

    Changes for Cancel Update Operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,22 +48,89 @@ export class AppComponent {
     this.empIdIndexBeforeUpdate = this.empData.findIndex((emp: any) => emp.id == empID);
 
     const hideObjectArray = ["spanName","spanDept","btnEdit","btnDelete"]; //hide span ID's
-    const showObjectArray = ["inputName","inputDept","update","btnDeleteDisable"]; //show input ID's
+    const showObjectArray = ["inputName","inputDept","update","btnCancel"]; //show input ID's
     
     hideObjectArray.map((item) => $("#"+item+empID).attr("hidden","hidden")); //Hide Span
     showObjectArray.map((item) => $("#"+item+empID).removeAttr("hidden")); //Show Input's
+
+    $("#delCol").attr("hidden","hidden"); //Hide Delete Heading
+    $("#cancelCol").removeAttr("hidden"); //Show Cancel Heading
+
+    //Logic to hide Delete button while Update in progress
+    this.empData.filter((item: { id: any; }) => item.id != empID)
+      .map((item: { id: any; }) => {return item.id})
+      .map((item: string) => $("#btnDelete"+item).attr("hidden","hidden"))
     
+    //Logic to hide edit button while Update in progress
+    this.empData.filter((item: { id: any; }) => item.id != empID)
+    .map((item: { id: any; }) => {return item.id})
+    .map((item: string) => $("#btnEdit"+item).attr("hidden","hidden"))  
+
+    //Logic to show disable edit button while Update in progress
+    this.empData.filter((item: { id: any; }) => item.id != empID)
+    .map((item: { id: any; }) => {return item.id})
+    .map((item: string) => $("#btnEditDisable"+item).removeAttr("hidden"))
+
+  }
+
+  cancelEdit(event: any){ //Cancel Update Operation
+
+    const empID = event.srcElement.id; //empID as reference to update data fields
+
+    //Hide Cancel button
+    $("#btnCancel"+empID).attr("hidden","hidden");
+
+    //Hide Update button
+    $("#update"+empID).attr("hidden","hidden");
+
+    //Logic to hide disable edit button while Update in progress
+    this.empData.map((item: { id: any; }) => {return item.id})
+    .map((item: string) => $("#btnEditDisable"+item).attr("hidden","hidden"))
+
+    //Logic to show Delete button
+    this.empData.map((item: { id: any; }) => {return item.id})
+                .map((item: string) => $("#btnDelete"+item).removeAttr("hidden"));
+    
+    //Logic to show edit button
+    this.empData.map((item: { id: any; }) => {return item.id})
+                .map((item: string) => $("#btnEdit"+item).removeAttr("hidden"))  
+
+    //Show Data back            
+    const showObjectArray = ["spanName","spanDept"]; //show span ID's
+    const hideObjectArray = ["inputName","inputDept"]; //hide inputs ID's
+    
+    hideObjectArray.map((item) => $("#"+item+empID).attr("hidden","hidden")); //Hide Input's
+    showObjectArray.map((item) => $("#"+item+empID).removeAttr("hidden")); //Show Span
+
+    $("#cancelCol").attr("hidden","hidden"); //Hide Cancel Heading
+    $("#delCol").removeAttr("hidden"); //Show Delete Heading
+
   }
 
   updateEmp(event: any){ //update employee Info
     
     const empID = event.srcElement.id; //empID as reference to update data
-    const showObjectArray = ["spanName","spanDept","btnEdit","btnDelete"]; //show span ID's
-    const hideObjectArray = ["inputName","inputDept","update","btnDeleteDisable"]; //hide inputs ID's
+    const showObjectArray = ["spanName","spanDept"]; //show span ID's
+    const hideObjectArray = ["inputName","inputDept"]; //hide inputs ID's
     
     hideObjectArray.map((item) => $("#"+item+empID).attr("hidden","hidden")); //Hide Input's
     showObjectArray.map((item) => $("#"+item+empID).removeAttr("hidden")); //Show Span
+
+    //Logic to hide disable edit button while Update in progress
+    this.empData.map((item: { id: any; }) => {return item.id})
+    .map((item: string) => $("#btnEditDisable"+item).attr("hidden","hidden"))
+
+    //Logic to show Delete button
+    this.empData.map((item: { id: any; }) => {return item.id})
+                .map((item: string) => $("#btnDelete"+item).removeAttr("hidden"));
     
+    //Logic to show edit button
+    this.empData.map((item: { id: any; }) => {return item.id})
+                .map((item: string) => $("#btnEdit"+item).removeAttr("hidden"))
+    
+    $("#cancelCol").attr("hidden","hidden"); //Hide Cancel Heading
+    $("#delCol").removeAttr("hidden"); //Show Delete Heading
+
     const updatedEmpName = $("#inputName"+empID).val(), //Get new data to update current Data
     updatedEmpDept = $("#inputDept"+empID).val();
 
@@ -101,7 +168,7 @@ export class AppComponent {
     const empIndex = this.empData.findIndex((emp: any) => emp.id == empID);
   
     //Confirm box for operation
-    const operationBool = confirm("Are you sure to delete "+this.empData[empIndex]["empName"]+"?") 
+    const operationBool = confirm("Are you sure you want to delete "+this.empData[empIndex]["empName"]+"?") 
     
     //Update local storage for Yes and alert for No
     operationBool === true ? 
